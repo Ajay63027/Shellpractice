bc283939041b3e7ec5f108399dd7859a076b5331
Ajay <ajaylearner01@gmail.com>
2025-05-17 16:03:17 +0530

    variables

---
+++ specialvariables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#if you want all variables passed to script: $@
+echo "number of variables passed: $#"
+echo "script name: $0"
+echo "present working directory: $PWD"
+echo "home directory of the user who is running the script: $HOME"
+echo "which user is running the script: $USER"
+echo "PID of the current script: $$"
+sleep 10 &
+echo "PID of the last command running in background: $!"
+echo "status of the last action performed=$?"
