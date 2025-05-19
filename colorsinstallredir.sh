f7713714dbc18a8f0ca16d67eb7f803f9df121a2
Ajay <ajaylearner01@gmail.com>
2025-05-19 17:17:32 +0530

    class16

--- colorsinstallredir.sh
+++ colorsinstallredir.sh
@@ -11,7 +11,7 @@ logfile="$logfolder/$script_name.log"
 
 mkdir -p $logfolder
 
-echo "script starting at $(date)" &>>$logfile
+echo "script starting at $(date)" | tee -a &>>$logfile
 
 if [ $uid -ne 0 ]
 then
@@ -24,9 +24,9 @@ fi
 VALIDATE(){
     if [ $1 -eq 0 ]
   then
-    echo -e "$2 installed  $G successfully $N" &>>$logfile
+    echo -e "$2 installed  $G successfully $N"  | tee -a &>>$logfile
   else 
-    echo -e "$2 installed $R failed $N" &>>$logfile
+    echo -e "$2 installed $R failed $N"  | tee -a &>>$logfile
   fi
 
 }
@@ -35,11 +35,11 @@ dnf list installed mysql &>>$logfile
 
 if [ $? -ne 0 ]
 then 
-echo -e "mysql $R not installed  $N" &>>$logfile
+echo -e "mysql $R not installed  $N"  | tee -a &>>$logfile
 dnf install mysql -y  &>>logfile
   VALIDATE $? "mysql" &>>logfile
 else 
-    echo -e "mysql is $Y already installed $N" &>>$logfile
+    echo -e "mysql is $Y already installed $N"  | tee -a &>>$logfile
 fi
  
 
@@ -47,12 +47,12 @@ dnf list installed python3 &>>$logfile
 
 if [ $? -ne 0 ]
 then 
-echo -e  "pythin3 $R not installed $N" &>>$logfile
+echo -e  "pythin3 $R not installed $N"  | tee -a &>>$logfile
 dnf install python3 -y  &>>$logfile
 VALIDATE $? "python3" &>>$logfile
   
 else 
-    echo -e "python3 is $Y already installed $N" &>>$logfile
+    echo -e "python3 is $Y already installed $N"  | tee -a &>>$logfile
 fi
 
 
@@ -60,9 +60,9 @@ dnf list installed nginx &>>$logfile
 
 if [ $? -ne 0 ]
 then 
-echo -e  "nginx $R not installed $N" &>>$logfile
+echo -e  "nginx $R not installed $N"  | tee -a &>>$logfile
 dnf install nginx -y &>>$logfile
   VALIDATE $? "nginx" &>>$logfile
 else 
-    echo -e "nginx is $Y  already installed $N" &>>$logfile
+    echo -e "nginx is $Y  already installed $N"  | tee -a &>>$logfile
 fi
