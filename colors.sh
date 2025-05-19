ed3a64421ef93501e7ef4543b8e9592c8d5faa44
Ajay <ajaylearner01@gmail.com>
2025-05-19 16:18:15 +0530

    class16

--- colors.sh
+++ colors.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 R="\e[31m"
-G=\e[32m
-Y=\e[33m
+G="\e[32m"
+Y="\e[33m"
+N= "]"
 
-echo -e "$R I am RED color"
+
+echo -e "$R I am RED color $N"
+echo -e "$G I am Green color $N"
+echo -e "$Y I am Yellow color $N"
