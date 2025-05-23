b6cdb087edb1d3cddc2280fa82e6392a746b5156
Ajay <ajaylearner01@gmail.com>
2025-05-23 19:48:15 +0530

    class20

--- deletelogs.sh
+++ deletelogs.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
+
+uid=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+logfolder="/var/log/shell_scriptlogs"
+script_name=$(echo $0 | cut -d "." -f1)
+logfile="$logfolder/$script_name.log"
 source_path=/home/ec2-user/app-logs
+
+
+if [ $uid -ne 0 ]
+then
+  echo "ERROR:: user have permisions to install" &>>$logfile
+  exit 1
+else
+  echo "user  have permissions to install" &>>$logfile
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+  then
+    echo -e "$2 installed  $G successfully $N"  | tee -a $logfile
+  else 
+    echo -e "$2 installed $R failed $N"  | tee -a $logfile
+  fi
+}
+    
+mkdir -p $logfolder
+
 files_old= $(find $source_path -name "*.log" -mtime "+14")
 
 
