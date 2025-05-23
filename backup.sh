aaaa6b72d60f3e9102c1f64688ccd078946b73bb
Ajay <ajaylearner01@gmail.com>
2025-05-23 21:13:08 +0530

    class20

---
+++ backup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+uid=$(id -u)
+source_dir=$1
+dest_dir=$2
+days=${$3:-14}
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+logfolder="/var/log/shell_scriptlogs"
+script_name=$(echo $0 | cut -d "." -f1)
+logfile="$logfolder/$script_name.log"
+
+mkdir -p $logfolder
+
+echo "script starting at $(date)" | tee -a $logfile
+
+if [ $uid -ne 0 ]
+then
+  echo "ERROR:: user have permisions to install" &>>$logfile
+  exit 1
+else
+  echo "user  have permissions to install" &>>$logfile
+fi
+USAGE(){
+    echo -e "$G USAGE:: $N Sh backup.sh <source_dir> <destination_dir> <days <days>(opt) "
+}
+
+if [ $# -lt 2 ]
+  then
+      USAGE
+  else
+fi 
+
