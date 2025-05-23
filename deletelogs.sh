349a6bf034bcb995a88642035941186dd3ed9ee3
Ajay <ajaylearner01@gmail.com>
2025-05-23 19:40:44 +0530

    class20

--- deletelogs.sh
+++ deletelogs.sh
@@ -3,7 +3,7 @@ source_path=/home/ec2-user/app-logs
 files_old= $(find $source_path -name "*.log" -mtime "+14")
 
 
-while IFS = read -r file
+while IFS= read -r file
 do
 rm -rf $file
 done <<< $files_old
