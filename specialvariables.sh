4d0e79d37b359175cfefdbb662f112ea909f12ba
Ajay <ajaylearner01@gmail.com>
2025-05-24 15:43:37 +0530

    class20

--- specialvariables.sh
+++ specialvariables.sh
@@ -8,5 +8,5 @@ echo "home directory of the user who is running the script: $HOME"
 echo "which user is running the script: $USER"
 echo "PID of the current script: $$"
 sleep 10 &
-echo "PID of the last command running in background: $!"
+echo "PID of the  the last command running in background: $!"
 echo "status of the last action performed=$?"
