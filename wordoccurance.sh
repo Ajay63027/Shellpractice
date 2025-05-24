ed8959f5ca7c225a60e08c901e050a40ba2e0f75
Ajay <ajaylearner01@gmail.com>
2025-05-24 16:06:49 +0530

    class20

--- wordoccurance.sh
+++ wordoccurance.sh
@@ -3,8 +3,7 @@
 filename=$1
 
 for word in $(cat "$filename"); do
-  echo "Word: $word"
   wordcount=$(grep -o -w "$word" "$filename" | wc -l)
-  echo "Count: $wordcount"
+  echo " Word: $word Count: $wordcount"
 done
 
