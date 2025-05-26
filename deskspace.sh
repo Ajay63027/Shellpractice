5c108a89380a3df14c8b8181d7ede0f0fcac3624
Ajay <ajaylearner01@gmail.com>
2025-05-26 19:29:49 +0530

    class20

--- deskspace.sh
+++ deskspace.sh
@@ -3,7 +3,7 @@
 diskusage=$(df -hT | grep -v Filesystem) 
 threshold=1
 
-while IFS= read -r line
+while IFS= read line
 do
  USAGE=$(echo $line | awk '{print $6F}'|cut -d '%' -f1)
  Partision=$(echo $line | awk '{print $7F}')
