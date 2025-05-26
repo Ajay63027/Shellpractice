6657766fca8e26f765c4e397e2c645fa1a07a1c9
Ajay <ajaylearner01@gmail.com>
2025-05-26 19:04:09 +0530

    class20

---
+++ deskspace.sh
@@ -0,0 +1,6 @@
+#!/bin/usr
+
+diskusage=$(df -hT | grep -v Filesystem|awk '{print $6F}'|cut -d '%' -f1) 
+filename=$(df -hT | grep -v Filesystem|awk '{print $7F}'|cut -d '%' -f1)
+
+echo "filename: $$filename diskusage:$diskusage"
