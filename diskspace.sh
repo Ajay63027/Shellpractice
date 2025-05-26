6779f63a43c0a38615beaf305db530e570ed5b85
Ajay <ajaylearner01@gmail.com>
2025-05-26 20:19:18 +0530

    class20

--- diskspace.sh
+++ diskspace.sh
@@ -2,6 +2,7 @@
 
 diskusage=$(df -hT | grep -v Filesystem) 
 threshold=1
+IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 
 while IFS= read line
 do
@@ -9,7 +10,9 @@ do
  Partision=$(echo $line | awk '{print $7F}') 
   if [ $USAGE -ge $threshold ] 
   then
-      MSG+="High disk usage: $Partision :$USAGE \n"
+      MSG+="High disk usage on : $Partision :$USAGE \n"
   fi
 done <<< $diskusage
 echo -e "$MSG"
+
+sh mail.sh DevOpsTeam  High_Diskspace $IP $MSG ajaymanthurthi6@gmail.com High_Disk_usage
