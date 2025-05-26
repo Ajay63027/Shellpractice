21883150f1949eb1b1f9eb061198f7e93be7e2dc
Ajay <ajaylearner01@gmail.com>
2025-05-26 19:12:57 +0530

    class20

--- deskspace.sh
+++ deskspace.sh
@@ -1,6 +1,13 @@
 #!/bin/usr
 
 diskusage=$(df -hT | grep -v Filesystem|awk '{print $6F}'|cut -d '%' -f1) 
-filename=$(df -hT | grep -v Filesystem|awk '{print $7F}'|cut -d '%' -f1)
+filename=$(df -hT | grep -v Filesystem|awk '{print $7F}')
 
 echo "filename: $filename diskusage:$diskusage"
+
+while IFS= read -r line
+do
+ USAGE=$(echo $diskusage | awk '{print $6F}'|cut -d '%' -f1)
+ Partision=$(echo $diskusage | awk '{print $7F}')
+ echo "diskusage :: $USAGE filename :: $partision"
+done <<< $diskusage
