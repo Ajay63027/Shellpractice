fafde8464852c07998792198b5af6c61cf3fb204
Ajay <ajaylearner01@gmail.com>
2025-05-26 19:21:25 +0530

    class20

--- deskspace.sh
+++ deskspace.sh
@@ -5,7 +5,7 @@ threshold=1
 
 while IFS= read -r line
 do
- USAGE=$(echo "$diskusage | awk '{print $6F}'|cut -d '%' -f1")
- Partision=$(echo "$diskusage | awk '{print $7F}'")
- echo "diskusage :: $USAGE filename :: $partision"
+ USAGE=$(echo $diskusage | awk '{print $6F}'|cut -d '%' -f1)
+ Partision=$(echo $diskusage | awk '{print $7F}')
+ echo "$USAGE $partision""
 done <<< $diskusage
